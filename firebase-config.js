511898c6228e961ddf47456451f4593481d63921
PeopleMac <peoplemac.inc@gmail.com>
2026-06-18 18:16:35 +0900

    Add Firebase Firestore sync (Spark plan compatible)

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+var FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCB-UKAo1nZ9hzBtEJZCADgzEfWmHirPNs",
+  authDomain:        "goinheaven-90c3e.firebaseapp.com",
+  projectId:         "goinheaven-90c3e",
+  storageBucket:     "goinheaven-90c3e.firebasestorage.app",
+  messagingSenderId: "103442976148",
+  appId:             "1:103442976148:web:bb04121291cea90ef17d381"
+};
